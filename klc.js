96951082d397be21524b946ab39859956d1ef19e
ManishNayak-123 <manishnayak5248@gmail.com>
2025-08-20 11:02:47 +0530

    Add files via upload

---
+++ klc.js
@@ -0,0 +1,28 @@
+//js part
+
+const display = document.querySelector("#display");
+let string = "";
+const btns = document.querySelectorAll(".btn");
+
+Array.from(btns).forEach(button =>{
+    button.addEventListener("click", (e) => {
+      if(e.target.innerHTML === "="){
+        string = eval(string);
+        document.querySelector("#display").value = string;
+      }
+      else if(e.target.innerHTML === "AC"){
+        string = "";
+        document.querySelector("#display").value = string;
+      }
+      // else if(e.target.innerHTML === "^"){
+      //   string = string*string;
+      //   document.querySelector("#display").value = string;
+      // }
+      else{
+     console.log(e.target);
+      string  = string + e.target.innerHTML;
+      document.querySelector("#display").value = string;
+      }
+    });
+
+});
